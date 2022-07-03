476adb63f76a0f447a96ed0f13d3faa3df9f7089
chikuwa111 <take0shun1111@gmail.com>
2022-07-04 00:46:24 +0900

    Fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/try-r3f/",
+  base: "/z-fighting-chikuwa/",
 });
